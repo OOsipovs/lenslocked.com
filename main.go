6cb45000add131752b0e849977ba5e00eb27ed45
Olegs <olegs.osipovs@gmail.com>
2020-12-28 17:42:07 +0200

    Creating ourbasic view

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awersome site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +22,34 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func notFound404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text-html")
+	fmt.Fprint(w, "<h1>FAQ</h1> if you have any question please select category below")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text-html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404</h1> sorry your page not found")
+}
+
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/", home)
+
 	http.ListenAndServe(":3000", r)
 }
