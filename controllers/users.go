68b923a7fabc7b4347e4cc9f188bfb6c37813592
Olegs <olegs.osipovs@gmail.com>
2021-01-17 17:17:52 +0200

    Parsing form with gorilla

--- controllers/users.go
+++ controllers/users.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/schema"
 
 	"../views"
 )
@@ -16,8 +19,30 @@ type Users struct {
 	NewView *views.View
 }
 
+// Getting signup screen
+// GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Create new user acccount
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
