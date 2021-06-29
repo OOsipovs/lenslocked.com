4b31b8a95bf223c56e1ea80089ad12a4deb8987c
Olegs <olegs.osipovs@gmail.com>
2021-06-29 20:29:54 +0300

    Rand package created

--- exp/main.go
+++ exp/main.go
@@ -3,53 +3,10 @@ package main
 import (
 	"fmt"
 
-	"../models"
-	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "olegsosipovs"
-	password = "141082"
-	dbname   = "lenslocked"
+	"lenslocked.com/rand"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-
-	us.DestructiveReset()
-	//defer us.Close()
-	user := models.User{
-		Name:  "Michael Scott",
-		Email: "michael@dundermifflin.com",
-	}
-
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	user.Email = "les.feerdinand@nw.co.uk"
-	if err := us.Update(&user); err != nil {
-		panic(err)
-	}
-
-	userByEmail, err := us.ByEmail("les.feerdinand@nw.co.uk")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(userByEmail)
-
-	userByID, err := us.ByID(user.ID)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(userByID)
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
